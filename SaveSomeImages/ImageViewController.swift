b4bad416bc48b669b38b378efd0ff13f3f8a03de
Xuezheng Wang <xzzwang@icloud.com>
2019-07-21 13:54:45 -0700

    Now user can tap can change image

--- SaveSomeImages/ImageViewController.swift
+++ SaveSomeImages/ImageViewController.swift
@@ -9,16 +9,53 @@
 import Foundation
 import UIKit
 
-class ImageViewController: UIViewController, UIImagePickerControllerDelegate {
+class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var theImage: UIImageView!
     @IBOutlet weak var theButton: UIButton!
+    @IBOutlet var tapRecognizer: UITapGestureRecognizer!
+    var pickerController = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
-            print(UIImagePickerController.availableMediaTypes(for: .savedPhotosAlbum))
+        theImage.tintColor = UIColor.gray
+        theImage.addGestureRecognizer(tapRecognizer)
+        theImage.isUserInteractionEnabled = true;
+        tapRecognizer.isEnabled = true
+        tapRecognizer.addTarget(self, action: #selector(handleTap(_:)))
+        
+        pickImage()
+    }
+    
+    
+    @IBAction func handleTap(_ sender: UITapGestureRecognizer) {
+        print("Called!")
+        if sender.state == .ended {
+            pickImage()
         }
+    }
+    
+    func pickImage(){
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
+            
+            pickerController.delegate = self
+            
+            if UIImagePickerController.availableMediaTypes(for: .savedPhotosAlbum)!.contains("public.image") {
+                pickerController.mediaTypes = ["public.image"]
+                self.present(pickerController, animated: true, completion: {})
+            }
+        }
+
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: {})
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        theImage.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage?
+        picker.dismiss(animated: true, completion: {})
     }
 }
